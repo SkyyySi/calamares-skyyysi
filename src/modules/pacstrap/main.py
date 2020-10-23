47f0f70a02a255880aef1f085ac1236b144d951e
SkyyySi <rasti.b@protonmail.com>
2020-10-24 01:21:09 +0200

    Updated severl files

--- src/modules/pacstrap/main.py
+++ src/modules/pacstrap/main.py
@@ -9,10 +9,10 @@ root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
     
 def run():
     """
-    Installing packages...
+    Installing packages. This may take a while.
     """
 
     PACSTRAP = "sudo /usr/bin/pacstrap_calamares"
-    PACKAGES = "amd-ucode base base-devel dhcpcd discover dolphin efibootmgr firefox firefox-ublock-origin firefox-dark-reader git grml-zsh-config grub gtk2 gtkmm3 konsole linux-firmware linux-zen linux-zen-headers nano networkmanager open-vm-tools os-prober packagekit-qt5 plasma-meta vlc xf86-input-elographics xf86-input-libinput xf86-input-vmmouse xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-openchrome xf86-video-sisusb xf86-video-vesa xf86-video-vmware xf86-video-voodoo xf86-video-qxl yay zsh"
+    PACKAGES = "amd-ucode base base-devel breeze-icons dracut efibootmgr firefox firefox-ublock-origin firefox-dark-reader git grml-zsh-config grub gtk2 gtkmm3 intel-ucode kvantum-qt5 pcmanfm linux-firmware linux-lts-zen linux-lts-headers nano networkmanager nitrogen nm-applet nordic-kde-git nordic-polar-theme-git nordic-theme-git open-vm-tools os-prober picom-ibhagwan-git plymouth termite vim vlc xf86-input-elographics xf86-input-libinput xf86-input-vmmouse xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-openchrome xf86-video-sisusb xf86-video-vesa xf86-video-vmware xf86-video-voodoo xf86-video-qxl yay zsh"
 
     subprocess.call(PACSTRAP.split(' ') + [root_mount_point] + PACKAGES.split(' '))
